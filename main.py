689d3eb6c88c64b826dc21e4cec181ac3f996f19
coders951 <71248284+coders951@users.noreply.github.com>
2026-05-25 14:25:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ df.to_csv(csv_buffer, index=False)
 # Upload to S3
 s3 = boto3.client(
     's3',
-    aws_access_key_id='YOUR_ACCESS_KEY',
-    aws_secret_access_key='YOUR_SECRET_KEY'
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
 )
 
 s3.put_object(
